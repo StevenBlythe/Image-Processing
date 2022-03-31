5215b98bff3a80db792723f85e377145bd75ea0b
StevenBlythe <StevenBlytheSU@gmail.com>
2022-03-31 10:28:36 -0400

    Alghs

--- Image Processing Algorithm/Program.cs
+++ Image Processing Algorithm/Program.cs
@@ -1,7 +1,22 @@
 ﻿using Image_Processing_Algorithm;
 
-Console.WriteLine("Hello, World!");
+// Image to modify
+string input = "";
+int menu;
+do
+{
+    try
+    {
+        Console.WriteLine("Select a menu item");
+        input = Console.ReadLine();
+        menu = int.Parse(input);
+    }
+    catch (System.FormatException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
 
-PDEAlgorithms.SelectImage(0);
 
-PDEAlgorithms.ReadImage()
+} while (input.ToLower() != "quit" && input.ToLower() != "exit" && input.ToLower() != "leave");
+
+
